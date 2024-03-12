a1c4643090efcadb974959d87b077858ad3126f8
sivareddyvara <145338813+sivareddyvara@users.noreply.github.com>
2024-03-12 12:13:44 +0530

    Update ss.py
    add code

--- ss.py
+++ ss.py
@@ -1,1 +1,22 @@
+class Siva:
+    def outer(self,f1):
+        def inner(self,a,b):
+            try:
+              result=f1(a,b)
+              return result
+            except ZeroDivisionError:
+              print('Division by zero accured')
+              return None
+        return inner
 
+    def f1(self,a,b):
+        return a/b
+s=Siva()
+a=s.f1(a,b)
+print(a)
+      
+      
+      
+  
+      
+      
